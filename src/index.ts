78ae9d8deab2397911851384b34a54f82a1c5363
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-01-28 13:56:15 +0300

    benchmarking native cluster mode ( # of cpus matter a lot)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import cluster from "cluster"
 import express from "express"
+
 const app = express()
 
 function doWork(msDuration: number) {
@@ -12,4 +14,26 @@ app.get("/", (req, res, next) => {
   doWork(5000)
   res.send("Hello World!")
 })
-app.listen(300)
+
+if (cluster.isPrimary) {
+  // Pimary (cluster manager) process
+  const numCPUs = require("os").cpus().length // get number of cores
+
+  console.log(`Master process ID: ${process.pid}`)
+
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    )
+  })
+} else {
+  // Worker process
+  app.listen(3000, () => {
+    console.log(`Worker ${process.pid} listening on port 3000`)
+  })
+}
