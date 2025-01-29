82938c48e2712009634b5f61483c65b9dbb5e237
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-01-29 15:35:57 +0300

    settings up redis & vscode workspace

--- src/redis/app.ts
+++ src/redis/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import redis from "redis"
 import mongoose from "mongoose"
 import textRouter from "./textRouter"
 
@@ -10,6 +11,18 @@ mongoose
   .connect("mongodb://localhost:27017/redis")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err))
+
+// connecting to Redis
+
+const redisClient = redis.createClient({
+  url: "redis://localhost:6379"
+})
+redisClient.set("name", "Ahmed")
+
+app.get("/", async (req, res) => {
+  const name = await redisClient.get("name")
+  res.send(`Hello, ${name}!`)
+})
 
 app.use(express.json())
 // Define a simple route to test the redis caching on the textModel resource
