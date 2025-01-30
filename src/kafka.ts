641e9a5a2ea3f413cf5d5cef312994716e1a5c54
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-01-30 09:05:56 +0300

    trying to implement kafka to add bidirectional communication between worker thread and main thread

---
+++ src/kafka.ts
@@ -0,0 +1,18 @@
+const { Kafka, ErrorCodes, CompressionTypes } =
+  require("@confluentinc/kafka-javascript").KafkaJS
+
+;async () => {
+  const producer = new Kafka().producer({
+    "bootstrap.servers": "<fill>"
+  })
+
+  await producer.connect()
+
+  const deliveryReports = await producer.send({
+    topic: "test-topic",
+    messages: [{ value: "v1", key: "x" }]
+  })
+
+  console.log({ deliveryReports })
+  await producer.disconnect()
+}
