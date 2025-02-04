8a195b1ec043314c69653fb66cdeb76e9867b8e5
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-02-04 23:08:08 +0300

    added a simple logger

--- src/nginx_reverseProxy_loadBalancer/server.ts
+++ src/nginx_reverseProxy_loadBalancer/server.ts
@@ -9,13 +9,14 @@ app.get("/api", async (req: Request, res: Response) => {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
+    console.log(`${process.pid}: handled the request`);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).send("An error occurred while fetching data.");
   }
 });
-const port = 3000;
-app.listen(port, () => {
+const port = (process.env.PORT as unknown as number) || 3000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
